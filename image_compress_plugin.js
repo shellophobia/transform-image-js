95736aaade697d6ad6aa9cd0a39728068ef46970
Suman <suman@plivo.com>
2016-11-24 19:10:57 +0530

    basic linting changes in file

--- image_compress_plugin.js
+++ image_compress_plugin.js
@@ -1,4 +1,12 @@
-var default_val = {
+////////////////---------- Upload Compress Image JS Plugin ---------------///////////////////
+
+// Author
+//---- Suman Anand
+// No Licensing till Now
+
+
+
+var defaultVal = {
   url: '',
   sizeLimit: 16777216,
   maxWidth: 500,
@@ -20,43 +28,43 @@ var default_val = {
 
 var uploadFile = function(options) {
   var self = this;
-  this.url = options.url || default_val['url'];
-  this.sizeLimit = options.sizeLimit || default_val['sizeLimit'];
-  this.maxWidth = options.maxWidth || default_val['maxWidth'];
-  this.maxHeight = options.maxHeight || default_val['maxHeight'];
-  this.quality = options.quality || default_val['quality'];
-  this.outputType = options.outputType || default_val['outputType'];
-  this.isBase64 = (typeof options.isBase64 === "undefined") ? default_val['isBase64'] : options.isBase64;
-  this.allowFileType = options.allowFileType || default_val['allowFileType'];
-  
-  this.enablePreview = (typeof options.enablePreview === "undefined") ? default_val['enablePreview'] : options.enablePreview;
+  this.url = options.url || defaultVal.url;
+  this.sizeLimit = options.sizeLimit || defaultVal.sizeLimit;
+  this.maxWidth = options.maxWidth || defaultVal.maxWidth;
+  this.maxHeight = options.maxHeight || defaultVal.maxHeight;
+  this.quality = options.quality || defaultVal.quality;
+  this.outputType = options.outputType || defaultVal.outputType;
+  this.isBase64 = (typeof options.isBase64 === "undefined") ? defaultVal.isBase64 : options.isBase64;
+  this.allowFileType = options.allowFileType || defaultVal.allowFileType;
+
+  this.enablePreview = (typeof options.enablePreview === "undefined") ? defaultVal.enablePreview : options.enablePreview;
   // if enable preview is enabled then specify the preview Selector
   this.previewSelector = options.previewSelector;
-  
-  this.inputFieldName = options.inputFieldName || default_val['inputFieldName'];
-  this.autoSubmit = (typeof options.autoSubmit === "undefined") ? default_val['autoSubmit'] : options.autoSubmit;
-  this.isDragNDrop = (typeof options.isDragNDrop === "undefined") ? default_val['isDragNDrop'] : options.isDragNDrop;
-  
-  this.appendFileInput = (typeof options.appendFileInput === "undefined") ? default_val['appendFileInput'] : options.appendFileInput;
+
+  this.inputFieldName = options.inputFieldName || defaultVal.inputFieldName;
+  this.autoSubmit = (typeof options.autoSubmit === "undefined") ? defaultVal.autoSubmit : options.autoSubmit;
+  this.isDragNDrop = (typeof options.isDragNDrop === "undefined") ? defaultVal.isDragNDrop : options.isDragNDrop;
+
+  this.appendFileInput = (typeof options.appendFileInput === "undefined") ? defaultVal.appendFileInput : options.appendFileInput;
   // if append file input is set to false then specify the input selector
-  this.fileInputSelector = options.fileInputSelector || default_val['fileInputSelector'];
-  
-  this.allowMultiple = (typeof options.allowMultiple === "undefined") ? default_val['allowMultiple'] : options.allowMultiple;
+  this.fileInputSelector = options.fileInputSelector || defaultVal.fileInputSelector;
+
+  this.allowMultiple = (typeof options.allowMultiple === "undefined") ? defaultVal.allowMultiple : options.allowMultiple;
   // if allow multiple is set to true
   this.maxFiles = options.maxFiles;
-  
+
   // allow ajax call to server
-  this.allowAjax = (typeof options.allowAjax === "undefined") ? default_val['allowAjax'] : options.allowAjax;
-  
-  this.appendFormData = (typeof options.appendFormData === "undefined") ? default_val['appendFormData'] : options.appendFormData;
+  this.allowAjax = (typeof options.allowAjax === "undefined") ? defaultVal.allowAjax : options.allowAjax;
+
+  this.appendFormData = (typeof options.appendFormData === "undefined") ? defaultVal.appendFormData : options.appendFormData;
   //append formdata from form
   this.formID = options.formID;
   //append formdata from string ["param1=x&param2=y"]
   this.formDataString = options.formDataString;
-  
+
   // no op
   this.noop = function() { return; }
-  
+
   this.onLoad = options.onLoad || this.noop;
   this.beforeSubmit = options.beforeSubmit || this.noop;
   this.onSuccess = options.onSuccess || this.noop;
@@ -65,9 +73,9 @@ var uploadFile = function(options) {
   this.stopFlag = false;
   this.formDataArray = {};
   this.imageID = 0;
-  
+
   self.formData = new FormData();
-  
+
   // construct the input DOM
   var toAppend = '<span>Click on the div to upload<p>Or Drag n Drop the file</p></span><input type = "file" ' + (this.allowMultiple ? 'multiple = "true"' : '')  + ' style = "position:absolute;top:0;left:0;right:0;bottom:0;opacity:0;z-index:100;cursor:pointer;height:100%;width:100%;">';
   this.toAppend = options.toAppend || toAppend;
@@ -92,7 +100,7 @@ var uploadFile = function(options) {
     var fileinput = document.getElementById(this.fileInputSelector);
     fileinput.value = "";
   }
-  
+
   this.testFileType = function(fileType) {
     var accepted = false;
     for (var i=0; i < this.allowFileType.length; i++) {
@@ -100,18 +108,18 @@ var uploadFile = function(options) {
       if (regEx.test(fileType))
         accepted = true;
     }
-    return accepted; 
+    return accepted;
   }
-  
+
   this.processFile = function(file) {
     if (! this.testFileType(file.type)) {
       alert( "File "+ file.name +" is not an image." );
       return false;
     }
-    
+
     // fire the onload callback
     this.onLoad();
-    
+
     // read the files
     var reader = new FileReader();
 
@@ -120,38 +128,38 @@ var uploadFile = function(options) {
     reader.onload = function (event) {
       // blob stuff
       var blob = new Blob([event.target.result]); // create blob...
-      var image_size = blob.size;
+      var imageSize = blob.size;
       // attach a 16 mb limit to images
-      if (image_size > this.sizeLimit) {
+      if (imageSize > this.sizeLimit) {
         alert('Please upload an image of size less than ' + this.sizeLimit/(1024*1024) + 'MB');
       }
       window.URL = window.URL || window.webkitURL;
       var blobURL = window.URL.createObjectURL(blob); // and get it's URL
-      
+
       // helper Image object
       var image = new Image();
       image.src = blobURL;
       image.onload = function() {
         // have to wait till it's loaded
         var resized = self.resizeImg(image); // send it to canvas
-        var resized_blob;
-        
+        var resizedBlob;
+
         if (self.isBase64) {
           self.formData.append(self.inputFieldName + (self.allowMultiple ? '[]' : ''), resized);
         }
         else {
-          resized_blob = self.base64ToBlob(resized);
-          if (resized_blob.size > file.size) {
-            resized_blob = file;
+          resizedBlob = self.base64ToBlob(resized);
+          if (resizedBlob.size > file.size) {
+            resizedBlob = file;
           }
-          self.formData.append(self.inputFieldName + (self.allowMultiple ? '[]' : ''), resized_blob);
+          self.formData.append(self.inputFieldName + (self.allowMultiple ? '[]' : ''), resizedBlob);
         }
-        
+
         if (self.allowAjax) {
           if (self.autoSubmit) {
             self.submitFormData();
           } else {
-            self.formDataArray[self.imageID] = (self.isBase64 ? resized : resized_blob);
+            self.formDataArray[self.imageID] = (self.isBase64 ? resized : resizedBlob);
             self.imageID++;
           }
         }
@@ -165,25 +173,25 @@ var uploadFile = function(options) {
             document.getElementById(self.targetElem.substring(1)).appendChild(hiddenInput);
           }
         }
-        
+
       }
     };
   }
-  
+
   this.appendUserFormData = function() {
-    
+
     // serialize form data to get a string of form data
     if (typeof this.formID !== "undefined") { this.formDataString = $(this.formID).serialize(); }
-    
+
     this.formDataString = this.formDataString.split('&');
-    
+
     for (var i=0; i<this.formDataString.length; i++) {
       var keyValue = this.formDataString[i].split('=');
       this.formData.append(keyValue[0], keyValue[1]);
     }
-    
+
   }
-  
+
   this.appendImageData = function() {
     var keys = Object.keys(this.formDataArray);
     this.formData = new FormData();
@@ -191,7 +199,7 @@ var uploadFile = function(options) {
       this.formData.append(this.inputFieldName + (self.allowMultiple ? '[]' : ''), this.formDataArray[keys[i]]);
     }
   }
-  
+
   // post data function
   this.submitFormData = function() {
     if (this.stopFlag) return;
@@ -205,7 +213,7 @@ var uploadFile = function(options) {
       contentType: false
     }).done(function(data) { self.onSuccess(data) }).fail( function() { self.onFailure() });
   }
-  
+
   // starts the upload
   this.startUpload = function() {
     this.stopFlag = false;
@@ -218,14 +226,14 @@ var uploadFile = function(options) {
       this.submitFormData();
     }
   }
-  
+
   // stops the upload
   this.stopUpload = function() {
     this.stopFlag = true;
   }
-  
+
   this.resizeImg = function(img) {
-    
+
     var canvas = document.createElement('canvas');
 
     var width = img.width;
@@ -243,13 +251,13 @@ var uploadFile = function(options) {
         height = this.maxHeight;
       }
     }
-    
+
     // resize the canvas and draw the image data into it
     canvas.width = width;
     canvas.height = height;
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, width, height);
-    
+
     if (this.enablePreview) {
       var parentDiv = document.createElement('div');
       parentDiv.setAttribute('class', 'preview_container');
@@ -262,10 +270,10 @@ var uploadFile = function(options) {
       parentDiv.appendChild(button);
       previewSelector.appendChild(parentDiv);
     }
-    
+
     return canvas.toDataURL("image/" + this.outputType, this.quality); // get the data from canvas as 50% JPG (can be also PNG, etc.)
   }
-  
+
   this.base64ToBlob = function(base64, contentType, sliceSize) {
     contentType = contentType || '';
     sliceSize = sliceSize || 512; // uses 512 as packet size for efficient conversion
@@ -290,7 +298,7 @@ var uploadFile = function(options) {
     var blob = new Blob(byteArrays, {type: 'image/' + this.outputType});
     return blob;
   }
-  
+
   // removes the upload Area container from dom
   this.removeField = function() {
     this.stopUpload();
@@ -301,7 +309,7 @@ var uploadFile = function(options) {
       $(this.previewSelector).html('');
     }
   }
-  
+
   // read files from input
   $('body').on('change', '#' + this.fileInputSelector, function(e) {
     if ( !( window.File && window.FileReader && window.FileList && window.Blob ) ) {
@@ -310,7 +318,7 @@ var uploadFile = function(options) {
     }
     self.readFiles(e.currentTarget.files);
   });
-  
+
   // delete preview code
   $('body').on('click', '.delete_preview', function(e) {
     $(this).parent().remove();
@@ -319,11 +327,11 @@ var uploadFile = function(options) {
     var fileinput = document.getElementById(self.fileInputSelector);
     fileinput.value = "";
   });
-  
+
   if (this.isDragNDrop) {
     // code for drag and drop images
     var $uploadArea = $(this.targetElem);
-    
+
     // drag and drop feature integrated
     $uploadArea.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
       e.preventDefault();
@@ -338,7 +346,7 @@ var uploadFile = function(options) {
       self.readFiles(droppedFiles);
     });
   }
-  
+
   return this;
 };
 
